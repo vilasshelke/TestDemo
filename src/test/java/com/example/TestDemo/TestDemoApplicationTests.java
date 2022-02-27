ec1cd07cbb42b3066eab0d35ba1869a2cd2f59a2
Vilas <mr.vilasshelke@gmail.com>
2022-02-27 16:08:29 +0000

    Add

---
+++ src/test/java/com/example/TestDemo/TestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
